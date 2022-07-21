494686702403e004a96d51a8a06ecf3759a8172f
syrius527 <83915027+syrius527@users.noreply.github.com>
2022-07-21 23:41:10 +0900

    Create models 1 (#16)
    * .
    
    * add python-dotenv and create models
    
    * update models.py
    
    Co-authored-by: Seolgyumin <0212seol@gmail.com>

--- todotodo/models.py
+++ todotodo/models.py
@@ -1,37 +1,55 @@
+from tkinter import CASCADE
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 # Create your models here.
-
-class Persona(models.Model): 
-    persona_name = models.CharField(max_length=256)
-    persona_message = models.TextField()
+class Friendship(models.Model):
+    friend1_id = models.ForeignKey(User, on_delete=models.CASCADE) #sender
+    friend2_id = models.ForeignKey(User, on_delete=models.CASCADE) #receiver
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    author = models.ForeignKey(User, null=True, on_delete= models.CASCADE)
 
-    def update_date(self): # 나중에 수정할 때 사용
-        self.updated_at = timezone.now()
-        self.save()
+class Persona(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    message = models.TextField()
+    # emozi = 
+    created_at = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.persona_name
+class FriendshipRequest(models.Model):
+    friend1_id = models.ForeignKey(User, on_delete=models.CASCADE) #sender
+    friend2_id = models.ForeignKey(User, on_delete=models.CASCADE) #receiver
+    persona1_ids = ArrayField(models.ForeignKey(Persona, on_delete=CASCADE)) #sender's open persona list
+    persona2_ids = ArrayField(models.ForeignKey(Persona, on_delete=CASCADE)) #receiver's open persona list
+    created_at = models.DateTimeField(default=timezone.now)
+
+class PersonaPermission(models.Model):
+    friendship_id = models.ForeignKey(Friendship, on_delete=CASCADE)
+    persona_id = models.ForeignKey(Persona, on_delete=CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
 
 class Category(models.Model):
-    content = models.TextField()
-    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE) 
-
-class Todo(models.Model):
-    content = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    persona_id = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    color = models.CharField(max_length=6) #settings.py의 hex code
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(blank=True, null=True)
-    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE) 
 
-    def update_date(self): 
-        self.updated_at = timezone.now()
-        self.save()
+class Todo(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    start_date = models.DateField(timezone.now)
+    end_date = models.DateField(null=True, blank=True)
+    sender_id = models.ForeignKey(User)
 
-
+class TodoRequest(models.Model):
+    sender_id = models.ForeignKey(User, on_delete=CASCADE)
+    reciever_id = models.ForeignKey(User, on_delete=CASCADE)
+    todo_name = models.CharField(max_length=256)
+    todo_start_date = models.DateField(default=timezone.now)
+    todo_end_date = models.DateField(null=True, blank=True)
+    todo_id = models.ForeignKey(Todo, null=True, blank=True)
+    status = models.IntegerField(MinValueValidator(0), MaxValueValidator(2))
+    
