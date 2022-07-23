ac0d839e002ac39bc41fa88629d706a65a97575a
Ivy-So <lucy08012@gmail.com>
2022-07-24 04:07:17 +0900

    feature/issue-6 mainpage UI

--- todotodo/views.py
+++ todotodo/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request, id):
+def index(request):
     if request.method == 'GET':
 
         return render(request, 'todotodo/index.html')
+
+def home(request):
+    return
 
 def send_todo():
     return
