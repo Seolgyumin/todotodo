7ed2b9848a6c0ad4fb857f17a2575b4f374daa63
Ivy-So <lucy08012@gmail.com>
2022-07-24 13:57:01 +0900

    Revert "feature/issue-10 mypage UI"
    This reverts commit 5d20a4dae4a3998021e267ec2270b77311e938e7.

--- todotodo/urls.py
+++ todotodo/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include
 from todotodo import views
 
 urlpatterns = [
-    path('<int:id>/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('accounts/', include('accounts.urls')),
     path('mypage/', include('mypage.urls')),
     path('history/', include('history.urls')),
