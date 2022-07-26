4da14397bfe3fabbc4c084090edb94b3d4a85259
syrius527 <djlee1031@naver.com>
2022-07-27 04:14:45 +0900

    migrate

--- history/views.py
+++ history/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'history/history.html')
 
-def chat_index(request):
+def chat(request):
     return render(request, 'history/chat.html')
