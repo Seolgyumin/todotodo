8b44c39eed166e03d31120b6791a6f27fe27e288
Seolgyumin <102405709+Seolgyumin@users.noreply.github.com>
2022-07-27 04:00:33 +0900

    Mainviews (#37)
    * mainviews
    
    * ll

--- history/urls.py
+++ history/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from history import views
+from django.conf.urls import include
+
 
 urlpatterns = [
     path('', views.index, name='history_index'),
-    path('chat/', views.chat_index),
+    path('chat/', views.chat, name='chat'),
 ]
