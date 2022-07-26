9c8c1b49702ff347e4ec6755445b32e216ee3cd8
syrius527 <djlee1031@naver.com>
2022-07-27 04:05:28 +0900

    mypage front done (modal left)

--- mypage/static/mypage/js/mypage.js
+++ mypage/static/mypage/js/mypage.js
@@ -1,8 +1,29 @@
-const addPersonaButton = document.getElementById('add-persona-button')
-addPersonaButton.onclick = (e) => handleAddPersona(e);
+const addPersonaModal = document.getElementById("add-persona-modal")
+const updatePersonaModal = document.getElementById("update-persona-modal")
+const addCategoryModal = document.getElementById("add-category-modal")
 
-const handleAddPersona = (e) => {
-    e.addPersona();
+const showAddPersonaModal = () => {
+    console.log('per-modal');
+    addPersonaModal.classList.remove('hide');
 }
 
+const hideAddPersonaModal = () => {
+    addPersonaModal.classList.add('hide');
+}
+
+const showUpdatePersonaModal = () => {
+    updatePersonaModal.classList.remove('hide');
+}
+
+const hideUpdatePersonaModal = () => {
+    updatePersonaModal.classList.add('hide');
+}
+
+const showAddCategoryModal = () => {
+    addCategoryModal.classList.remove('hide');
+}
+
+const hideCategoryModal = () => {
+    addCategoryModal.classList.add('hide');
+}
 // 자스 전부 다 미완성임 싹 지우고 다시 해도 됨 index.html 과 연결
