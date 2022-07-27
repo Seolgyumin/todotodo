c1fb4710ac00cbf75193855853ec4a54c73599c8
Ivy-So <102405666+Ivy-So@users.noreply.github.com>
2022-07-28 03:04:00 +0900

    Fix kakao callback (#40)

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 import os
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -13,6 +15,19 @@ def kakao_login(request):
     return redirect(f'{kakao_auth_api}client_id={client_id}&redirect_uri={redirect_uri}&response_type=code')
 
 def kakao_callback(request):
+    auth_code = request.GET.get('code')
+    kakao_token_api = 'https://kauth.kakao.com/oauth/token'
+    data = {
+        'grant_type': 'authorization_code',
+        'client_id': os.environ.get('KAKAO_REST_API_KEY'),
+        'redirection_uri': 'http://localhost:8000/accounts/signin/kakao/callback',
+        'code': auth_code
+    }
+
+    token_response = requests.post(kakao_token_api, data=data)
+    access_token = token_response.json().get('access_token')
+    user_info_response = requests.get('https://kapi.kakao.com/v2/user/me', headers={"Authorization": f'Bearer ${access_token}'})
+
     return redirect('/accounts/signin')
 
 def signin(request):
