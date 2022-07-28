955fccc58692e30e6cdddb42553de862ea31f547
Pyungjae Park <78852778+pyungjae9492@users.noreply.github.com>
2022-07-29 00:21:44 +0900

    Feature/kakao user (#42)
    * Add kakao and jwt feature
    
    * Change html naming
    
    Co-authored-by: pyungjae-park <pyungjae.park@ringleplus.com>

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from todotodo.models import Friendship
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
+
+class KakaoUser(models.Model):
+    name = models.CharField(max_length=256, default='', verbose_name='사용자명')
+    email = models.EmailField(max_length=256, verbose_name='사용자이메일', blank=True, null=True)
+    thumbnail_image_url = models.TextField(verbose_name='썸네일 이미지 URL', blank=True, null=True)
+    profile_image_url = models.TextField(verbose_name='썸네일 이미지 URL', blank=True, null=True)
+    connected_at = models.DateTimeField(default=timezone.now)
+
 class Profile(models.Model):
     name = models.CharField(max_length=256, default='')
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    friends = models.ManyToManyField("self", symmetrical=False, through=Friendship, related_name='friendship')
+    user = models.OneToOneField(KakaoUser, on_delete=models.CASCADE)
+    friends = models.ManyToManyField("self", symmetrical=False, through="todotodo.Friendship", related_name='friendship')
     created_at = models.DateTimeField(default=timezone.now)
 
     @receiver(post_save, sender=User)  
