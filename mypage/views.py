509f0faa48dfb7c78b9a043def2e188b488f9410
syrius527 <djlee1031@naver.com>
2022-07-30 05:20:34 +0900

    restore mypage...

--- mypage/views.py
+++ mypage/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from todotodo.models import Persona
+from todotodo.models import Persona, Category
 
 def index(request):
-    personas = Persona.objects.all()
-    return render(request, 'mypage/index.html', {'personas': personas})
+    user = request.user
+    personas = Persona.objects.filter(user=user)
+    persona_categories = []
+
+    for persona in personas:
+        each = {}
+        each['persona'] = persona
+        each['categories'] = Category.objects.filter(persona=persona)
+        persona_categories.append(each)
+
+    print(persona_categories)
+    return render(request, 'mypage/index.html', {'personas': persona_categories})
 
 def friends_list(request):
     return render(request, 'mypage/friends_list.html')
